a7b03e66f4c9eb5314968688e3684dea5e4ef873
yadavhemantyadav <Hemant@Hemants-Macbook.local>
2017-10-12 16:20:11 +0530

    kd api changes

--- app.js
+++ app.js
@@ -1,23 +1,121 @@
 //require('newrelic');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var mysql = require('mysql');
+var dateTime = require('node-datetime');
+
+var cors = require('cors');
+ 
+app.use(cors());
+
+// var bodyParser = require("body-parser");
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// var db_config = {
+//     host: "ec2-13-59-131-1.us-east-2.compute.amazonaws.com",
+//     user: "hemant",
+//     password: "abc@abc",
+//     database: "kd"
+// };
+
+var mysql_pool  = mysql.createPool({
+  connectionLimit : 100,
+  host            : 'ec2-13-59-131-1.us-east-2.compute.amazonaws.com',
+  user            : 'hemant',
+  password        : 'abc@abc',
+  database        : 'kd'
+});
+
+
+// function handleDisconnect() {
+// 	console.log('handleDisconnect()');
+// 	connection.destroy();
+// 	connection = mysql.createConnection(db_config);
+// 	connection.connect(function(err) {
+// 	    if(err) {
+// 			console.log(' Error when connecting to db  (DBERR001):', err);
+// 			setTimeout(handleDisconnect, 1000);
+// 	    }
+// 	});
+
+// }
+
+// var connection = mysql.createConnection(db_config);
+
+app.use(express.static(__dirname + '/public/'));
+
 console.log("starting");
 var port = process.env.PORT || 5000;
 process.env.PWD = process.cwd();
 
 var compression = require('compression');
-var express = require("express");
-var http = require("http");
-var app = express();
-
 app.use(compression());
 
 app.use(express.static(process.env.PWD));
 
-app.use(express.static(__dirname + '/public/'));
+app.get('/timeTable',function(request,response,next){
+	var dt = dateTime.create();
+	var formatted = dt.format('d.m.Y');
+	console.log(formatted);
 
-app.get("/", function(request, response) {
-    response.send('Hey!!');
+	var query = "select * from TimeTableDetails where dates = '" + formatted + "'";
+	//var query = "select * from TimeTableDetails";
 
+	console.log(formatted);
+
+	mysql_pool.getConnection(function(err, connection) {
+		if (err) {
+			connection.release();
+	  		console.log(' Error getting mysql_pool connection: ' + err);
+	  		throw err;
+	  	}
+	    connection.query(query, function(err2, rows) {	
+	    	if (err2) {
+	    		console.log("error in query");
+				var data = { "Time":"", "DatabaseStatus":"" };
+				data["Time"] = (new Date()).getTime();
+				data["DatabaseStatus"] = "Down";
+				data["error_code"] = 1;
+				response.json(data); 
+			} else {
+				console.log(rows);
+				data = {};
+				arraydata = {};
+				for(var i=0; i<rows.length; i++){
+					data[i] = rows[i];
+				}
+				console.log(data);
+				res={};
+				res.table = data;
+				res.error_code = "1";
+				console.log(res);
+				response.json(res);
+			}
+			console.log(' mysql_pool.release()');
+			connection.release();
+	    });
+	});
 });
+
+// app.get("/", function(request, response,next) {
+    
+// });
+
+app.get('*', function (req, res) {
+    console.log(req.params);
+    //    var response = {};
+    //    response["message"] = "invalid url";
+    //    response["error_code"] = 1;
+    //    res.end(JSON.stringify(response));
+    res.sendFile(__dirname + '/index.html');
+});
+
 
 // for preventing dyno to sleeping  in every 3 minutes (180000)
 // setInterval(function() {
--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,15 @@ var app = angular.module('Portfolio', ['ngRoute']);
         $routeProvider.when('/',{
            templateUrl:'view/home.html',
             controller:'PortFolioCtrl'
+        })
+        .when("/kdTimeTable", {
+                 templateUrl: "view/timetable.html"
+             })
+        .when("/error", {
+                 templateUrl: "view/error.html"
+        })
+        .otherwise({
+                 templateUrl: "view/error.html"
         });
     });
 
@@ -12,6 +21,46 @@ var app = angular.module('Portfolio', ['ngRoute']);
 app.controller('PortFolioCtrl', function($scope){
 
 });
+
+app.controller('table', function($scope, baseURL, $http, $location){
+	$scope.baseURL = baseURL.getProperty();
+
+	$scope.load = function () {
+
+		$http.get($scope.baseURL + "/timeTable", {
+                 cache: false
+             })
+             .success(function (response) {
+             	console.log(response);
+                 if (response.error_code!="0") {
+                     //$location.path('/error');
+                     console.log("inside" + response);
+                     $scope.response = response.table;
+                     console.log($scope.response);
+
+                	console.log($scope.response.table);	
+                 } else {
+                     $scope.response = {"error":2};
+                     console.log($scope.response);
+                 }
+             });
+
+	};
+	
+	$scope.load();
+});
+
+app.service('baseURL', function () {
+         var property = "http://192.168.0.192:5000";
+
+         return {
+             getProperty: function () {
+                 return property;
+             }
+         };
+     });
+
+
 
 
 //========================      Jquery     =======================================
